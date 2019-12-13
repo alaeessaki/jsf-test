6a25bf58ab22947926929446fd3232451e16aab8
Alae Es-saki <alaeessaki@gmail.com>
2019-12-13 11:27:24 +0100

    init commit

---
+++ src/main/java/jsftest/managedBeans/PersonBean.java
@@ -0,0 +1,35 @@
+package jsftest.managedBeans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class PersonBean {
+	private int id;
+	private String nom;
+	private String prenom;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+}
